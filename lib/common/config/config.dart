4f515e3d0233012f51eb484699b107c0dd94d19d
wangsw <wangsw@ushareit.com>
2020-01-10 20:58:22 +0800

    Fix ios

--- lib/common/config/config.dart
+++ lib/common/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const BASE_API_URL = 'http://192.168.9.124:3000';
+//  static const BASE_API_URL = 'http://192.168.9.124:3000';
 //  static const BASE_API_URL = 'http://192.168.0.109:3000';
-//  static const BASE_API_URL = 'http://10.20.4.11:3000';
+  static const BASE_API_URL = 'http://10.20.4.11:3000';
   static const DEBUG = true;
 }
